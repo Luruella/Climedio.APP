e0c6534000bf6b2f719ebe7437dba7567516ea0c
Wagner Donizete <wagnerdoniizete@outlook.com>
2025-01-29 17:43:19 -0300

    29/01/2025 segundo

--- climediofront/src/App.js
+++ climediofront/src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Home } from './paginas/Home/Home';
+import { Usuarios } from './paginas/Home/Usuarios/Usuarios';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/home' element={<Home />} />
+        <Route path='/usuarios' element={<Usuarios />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
-
 export default App;
